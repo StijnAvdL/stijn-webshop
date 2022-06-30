71ef694d469e5781077ee5346028692e9060c3f9
Stijn Albers van der Linden <stijn.albersvanderlinden@weareyou.com>
2022-06-30 10:35:02 +0200

    split op code nicely

--- storyblok/ProductList.vue
+++ storyblok/ProductList.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="products" class="grid grid-cols-1 md:grid-cols-2 md:gap-2 mx-auto container">
     <div v-for="product in products" :key="product.id">
-      <Item :product="product" />
+      <Product :product="product" />
     </div>
   </div>
 </template>
@@ -9,7 +9,6 @@
 <script lang="ts">
 import { storeToRefs } from "pinia";
 import { useProductStore } from "../stores/ProductStore";
-import Item from "./Item.vue";
 
 export default {
   async setup() {
@@ -23,6 +22,5 @@ export default {
       products,
     };
   },
-  components: { Item },
 };
 </script>
