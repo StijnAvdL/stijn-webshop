bf06da9eee27c4ff100eea61f4daf1d97c6b3eed
Stijn Albers van der Linden <stijn.albersvanderlinden@weareyou.com>
2022-07-04 15:58:50 +0200

    try fix for netilfy

--- components/Header.vue
+++ components/Header.vue
@@ -18,6 +18,8 @@
 </template>
 
 <script setup>
+import { useStoryblokApi } from "@storyblok/vue";
+
 const storyblokApi = useStoryblokApi();
 const { data } = await storyblokApi.get("cdn/stories/menu", {
   version: "draft",
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,14 @@
 import { defineNuxtConfig } from 'nuxt'
-import svgLoader from 'vite-svg-loader'
+// import svgLoader from 'vite-svg-loader'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    // https://nuxtjs.org/docs/configuration-glossary/configuration-target/
+    target: 'static',
     modules: [
         ['@storyblok/nuxt', { accessToken: process.env.STORYBLOK_ACCESS_TOKEN }],
         '@nuxtjs/tailwindcss',
         '@pinia/nuxt',
     ],
-    vite: {
-        plugins: [svgLoader()]
-    }
+    vite: false
 })
